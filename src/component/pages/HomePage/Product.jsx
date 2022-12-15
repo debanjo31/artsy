08d4e55734b41019016050dea01469c4102ee82a
debanjo31 <debanjo31@gmail.com>
2022-12-15 16:50:20 +0100

    featured project

--- src/component/pages/HomePage/Product.jsx
+++ src/component/pages/HomePage/Product.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { FaLongArrowAltRight} from "react-icons/fa";
+import '../homepage.css';
 import Person1 from './img/person1.png'
 import Person2 from './img/person2.png'
 import Person3 from './img/person3.png'
 import Person4 from './img/person4.png'
 import Person5 from './img/person5.png'
+import arrowVector from './img/Vector.png'
 function Product({img}) {
   return (
     <div className='product'>
@@ -22,8 +23,8 @@ function Product({img}) {
                   <img src={Person4} alt="Person4" />
                   <img src={Person5} alt="Person5" />
                 </span>
-                <span>64 major creator</span>
-                <FaLongArrowAltRight />
+                <span className='imageText'>64 major creator</span>
+                <span className="prodcutarrow"><img src={arrowVector} alt="arrowicon" className='arrowicon' /></span>
             </div>
         </div>
     </div>
